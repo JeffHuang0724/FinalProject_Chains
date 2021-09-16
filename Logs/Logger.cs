46c5570548f422eb160f8ff9792b990b66dae7f8
Jeff Huang <85493578+JeffHuang0724@users.noreply.github.com>
2021-09-16 14:40:52 +0800

    Add files via upload

---
+++ Logs/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject_Chains.Logs
+{
+    public class Logger
+    {
+        public static void WriteLog(Exception ex)
+        {
+            string msg = $@" {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}";
+            msg += $@" {ex.ToString()}";
+            msg += "\r\n--------------------------------------------------------\r\n";
+
+            string folderPath = System.Configuration.ConfigurationManager.AppSettings["logFolderPath"];
+            string logPath = $"{folderPath}\\Logs.log";
+
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            if (!System.IO.File.Exists(logPath))
+                System.IO.File.Create(logPath);
+
+            System.IO.File.AppendAllText(logPath, msg);
+        }
+    }
+}
